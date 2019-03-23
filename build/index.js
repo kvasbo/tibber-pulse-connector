12e4c72c13d226b95bbe0257681320f7aa9e1507
kvasbo <audun@kvasbo.no>
2019-03-23 20:11:22 +0100

    Setting up!

--- build/index.js
+++ build/index.js
@@ -2,19 +2,54 @@
 
 var _apolloLinkHttp = require("apollo-link-http");
 
+var _apolloLinkWs = require("apollo-link-ws");
+
+var _subscriptionsTransportWs = require("subscriptions-transport-ws");
+
 var _apolloCacheInmemory = require("apollo-cache-inmemory");
 
 var _apolloClient = _interopRequireDefault(require("apollo-client"));
+
+var _graphqlTag = _interopRequireDefault(require("graphql-tag"));
+
+var _ws = _interopRequireDefault(require("ws"));
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Polyfill
 if (!global.fetch) global.fetch = require('node-fetch');
 var ENDPOINT = "wss://api.tibber.com/v1-beta/gql/subscriptions";
+/*
+const client = new SubscriptionClient(ENDPOINT, {
+  reconnect: true
+});
+*/
+
+var link = new _apolloLinkWs.WebSocketLink({
+  uri: ENDPOINT,
+  options: {
+    reconnect: true,
+    connectionParams: function connectionParams() {
+      return {
+        authToken: '1a3772d944bcf972f1ee84cf45d769de1c80e4f0173d665328287d1e2a746004'
+      };
+    }
+  },
+  webSocketImpl: _ws.default
+});
+/*
+const link = new WebSocketLink({ uri: ENDPOINT, options: {}, webSocketImpl: ws });
+*/
+
 var apolloClient = new _apolloClient.default({
-  link: new _apolloLinkHttp.HttpLink(ENDPOINT),
+  link: link,
   cache: new _apolloCacheInmemory.InMemoryCache()
 });
+console.log("client created");
+/*
+apolloClient.query({ query: gql`{ hello }` }).then(console.log);
+*/
+
 /*
 apolloClient.onConnected((a) => {
   console.log('connected', a);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,49 @@
-import { HttpLink } from 'apollo-link-http';
+import { HttpLink, createHttpLink } from 'apollo-link-http';
+import { WebSocketLink } from 'apollo-link-ws';
+import { SubscriptionClient } from "subscriptions-transport-ws";
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import ApolloClient from 'apollo-client';
+import gql from 'graphql-tag';
+import ws from 'ws';
 
 // Polyfill
 if (!global.fetch) global.fetch = require('node-fetch');
 
 const ENDPOINT = `wss://api.tibber.com/v1-beta/gql/subscriptions`;
 
+/*
+const client = new SubscriptionClient(ENDPOINT, {
+  reconnect: true
+});
+*/
+
+const link = new WebSocketLink({
+  uri: ENDPOINT,
+  options: {
+    reconnect: true,
+    connectionParams: () => ({
+      authToken: '1a3772d944bcf972f1ee84cf45d769de1c80e4f0173d665328287d1e2a746004',
+    }),
+  },
+  webSocketImpl: ws
+});
+
+/*
+const link = new WebSocketLink({ uri: ENDPOINT, options: {}, webSocketImpl: ws });
+*/
+
+
 const apolloClient = new ApolloClient({
-  link: new HttpLink(ENDPOINT),
+  link: link,
   cache: new InMemoryCache()
 });
+
+
+console.log("client created");
+
+/*
+apolloClient.query({ query: gql`{ hello }` }).then(console.log);
+*/
 
 /*
 apolloClient.onConnected((a) => {
