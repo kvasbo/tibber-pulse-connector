dcd422e00cfab03e00878588306bbf4b0d17de4b
kvasbo <audun@kvasbo.no>
2019-03-28 11:12:49 +0100

    Cleanup and project init

--- src/index.js
+++ src/index.js
@@ -8,11 +8,8 @@ import ws from 'ws';
 if (!global.fetch) global.fetch = require('node-fetch');
 
 const ENDPOINT = `wss://api.tibber.com/v1-beta/gql/subscriptions`;
-// const HOMEID = `68e6938b-91a6-4199-a0d4-f24c22be87bb`;
-// const TOKEN = `d1007ead2dc84a2b82f0de19451c5fb22112f7ae11d19bf2bedb224a003ff74a`;
-
-const HOMEID = "68e6938b-91a6-4199-a0d4-f24c22be87bb"
-const TOKEN = "d1007ead2dc84a2b82f0de19451c5fb22112f7ae11d19bf2bedb224a003ff74a"
+const TOKEN = `d1007ead2dc84a2b82f0de19451c5fb22112f7ae11d19bf2bedb224a003ff74a`;
+const HOMEID = `68e6938b-91a6-4199-a0d4-f24c22be87bb`;
 
 const CONSUMPTION_QUERY = gql`subscription liveConsumption($homeId: ID!) {
   liveMeasurement(homeId:$homeId)
