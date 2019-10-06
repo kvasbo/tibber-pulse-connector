578bd4ce9b7a8da99d35cae9c52196470463d8af
kvasbo <audun@kvasbo.no>
2019-10-06 12:24:28 +0200

    Fix test

--- test/index.js
+++ test/index.js
@@ -2,8 +2,8 @@ const ws = require('ws');
 const tibber = require('../dist/index');
 
 // Test tokens
-const TOKEN = `d1007ead2dc84a2b82f0de19451c5fb22112f7ae11d19bf2bedb224a003ff74a`;
-const HOMEID = `c70dcbe5-4485-4821-933d-a8a86452737b`;
+const token = `d1007ead2dc84a2b82f0de19451c5fb22112f7ae11d19bf2bedb224a003ff74a`;
+const homeId = `c70dcbe5-4485-4821-933d-a8a86452737b`;
 
-const connector = new tibber(TOKEN, HOMEID, (data) => { console.log(data) }, ws);
+const connector = new tibber({token, homeId, onData: (data) => { console.log(data) }, ws});
 connector.start();
