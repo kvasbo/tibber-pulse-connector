79c2f01c2fc499e861474acd5a1d32dcd9d88bf2
kvasbo <audun@kvasbo.no>
2019-11-01 12:18:46 +0100

    Add automated test

--- test/index.js
+++ test/index.js
@@ -1,10 +1,26 @@
 const ws = require('ws');
 const tibber = require('../dist/index');
+let datas = 0;
 
 // Test tokens
 const token = `d1007ead2dc84a2b82f0de19451c5fb22112f7ae11d19bf2bedb224a003ff74a`;
-const homeId = `c70dcbe5-4485-4821-933d-a8a86452737b`;
-// const homeId = [`c70dcbe5-4485-4821-933d-a8a86452737b`, `c70dcbe5-4485-4821-933d-a8a86452737b`];
+const homeId = [`c70dcbe5-4485-4821-933d-a8a86452737b`];
 
-const connector = new tibber({token, homeId, onData: (data, id) => { console.log(id, data) }, ws});
+// Give up test after 30s.
+setTimeout(() => process.exit(1), 30000);
+
+function gotData(id, data) {
+  datas += 1;
+  console.log(id, data)
+  if (datas > 3) {
+     process.exit(0);
+  }
+}
+
+function gotError(id, err) {
+  console.error(id, err)
+  process.exit(1);
+}
+
+const connector = new tibber({token, homeId, onData: (data, id) => { gotData(id, data) }, onError: (error, id) => { gotError(id, error) }, ws});
 connector.start();
